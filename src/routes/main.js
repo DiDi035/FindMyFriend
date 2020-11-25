cb0208e36862230f2b26165de448ed4fc5ce0413
Lisu35 <baodihuynh@gmail.com>
2020-11-25 22:27:07 +0700

    pet detail

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,17 +1,22 @@
 const express = require("express");
 
 const requiredLogIn = require("../middleware/RequiredLogin");
+const Product = require("../database/models/Product");
 const router = express.Router();
 
 router.get("/", requiredLogIn, (req, res) => {
   if (!req.session.user_id) {
     res.redirect("/auth/login");
   }
-  res.render("MainView");
+  const allPets = Product.find();
+  res.render("MainView", { allPets });
 });
 
-router.get("/:animalId", (req, res) => {
-  
-})
+router.get("/:animal/:breed", requiredLogIn, async (req, res) => {
+  const { animal, breed } = req.params;
+  const chosenPets = await Product.find({ animal, breed });
+  res.render("MainView", { chosenPets });
+  // res.send(animal + " " + breed);
+});
 
 module.exports = router;
