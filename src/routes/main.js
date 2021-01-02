f72da9575e996c5b46d9db681d41a309f67b5258
Lisu35 <baodihuynh@gmail.com>
2021-01-02 17:23:42 +0700

    92 cai commit cua tg loz Nu

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,68 +5,47 @@ const { Product } = require("../database/models/Product");
 const User = require("../database/models/User");
 const router = express.Router();
 
-router.get("/", requiredLogIn, async (req, res) => {
-  const allPets = await Product.find({ isHide: false });
-  const shop = req.shop;
-  const user = req.user;
-  
-  res.render("MainView", { allPets, shop, user });
+router.get("/:page", requiredLogIn, async (req, res) => {
+    const { page } = req.params;
+    const pageNum = parseInt(page);
+    const pets = await Product.find({ isHide: false });
+    const pages = Math.trunc(pets.length / 9) + 1;
+    let allPets = [];
+    if ((pageNum - 1) * 9 < pets.length) {
+        if ((pageNum - 1) * 9 + 8 < pets.length) {
+            allPets = pets.slice((pageNum - 1) * 9, (pageNum - 1) * 9 + 9);
+        } else {
+            allPets = pets.slice((pageNum - 1) * 9, pets.length);
+        }
+    }
+    const shop = req.shop;
+    const user = req.user;
+    res.render("MainView", { allPets, shop, user, pageNum, pages });
 });
 
-router.get("/:animal/:breed", requiredLogIn, async (req, res) => {
-  const { animal, breed } = req.params;
-  const allPets = await Product.find({ animal, breed });
-  const shop = req.shop;
-  const user = req.user;
-  res.render("MainView", { allPets, shop, user });
-}
-
-router.get("/customer/:page", requiredLogIn, async (req, res) => {
-  const { page } = req.params;
-  const pageNum = parseInt(page);
-  const pets = await Product.find({ isHide: false });
-  const pages = Math.trunc(pets.length / 9) + 1;
-  let allPets = [];
-  if ((pageNum - 1) * 9 < pets.length) {
-    if ((pageNum - 1) * 9 + 8 < pets.length) {
-      allPets = pets.slice((pageNum - 1) * 9, (pageNum - 1) * 9 + 9);
-    } else {
-      allPets = pets.slice((pageNum - 1) * 9, pets.length);
+router.get("/:breed/:page", requiredLogIn, async (req, res) => {
+    const shop = req.shop;
+    const user = req.user;
+    const { breed, page } = req.params;
+    const pageNum = parseInt(page);
+    const pets = await Product.find({ breed, isHide: false });
+    const pages = Math.trunc(pets.length / 9) + 1;
+    let allPets = [];
+    if ((pageNum - 1) * 9 < pets.length) {
+        if ((pageNum - 1) * 9 + 8 < pets.length) {
+            allPets = pets.slice((pageNum - 1) * 9, (pageNum - 1) * 9 + 9);
+        } else {
+            allPets = pets.slice((pageNum - 1) * 9, pets.length);
+        }
     }
-  }
-  const curUser = await User.findById(req.session.user_id);
-  res.render("MainView", {
-    allPets,
-    pageNum,
-    pages,
-    breed: undefined,
-    curUser,
-    curUserType: "customer",
-  });
-});
-
-router.get("/customer/:breed/:page", requiredLogIn, async (req, res) => {
-  const { breed, page } = req.params;
-  const pageNum = parseInt(page);
-  const pets = await Product.find({ breed, isHide: false });
-  const pages = Math.trunc(pets.length / 9) + 1;
-  let allPets = [];
-  if ((pageNum - 1) * 9 < pets.length) {
-    if ((pageNum - 1) * 9 + 8 < pets.length) {
-      allPets = pets.slice((pageNum - 1) * 9, (pageNum - 1) * 9 + 9);
-    } else {
-      allPets = pets.slice((pageNum - 1) * 9, pets.length);
-    }
-  }
-  const curUser = await User.findById(req.session.user_id);
-  res.render("MainView", {
-    allPets,
-    pageNum,
-    pages,
-    breed,
-    curUser,
-    curUserType: "customer",
-  });
+    res.render("MainView", {
+        allPets,
+        pageNum,
+        pages,
+        breed,
+        user,
+        shop,
+    });
 });
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -11,102 +11,119 @@ const User = require("../database/models/User");
 
 // Check if uploaded file type valid
 const checkFileType = (file, cb) => {
-  const fileType = /jpeg|jpg|png|gif/;
-  const extName = fileType.test(path.extname(file.originalname));
-  const mimeType = fileType.test(file.mimetype);
-  if (mimeType && extName) {
-    return cb(null, true);
-  } else {
-    cb("ERROR: This file is not an image");
-  }
+    const fileType = /jpeg|jpg|png|gif/;
+    const extName = fileType.test(path.extname(file.originalname));
+    const mimeType = fileType.test(file.mimetype);
+    if (mimeType && extName) {
+        return cb(null, true);
+    } else {
+        cb("ERROR: This file is not an image");
+    }
 };
 
 router.get("/:userId/profile", requiredLogIn, async (req, res) => {
-  const { userId } = req.params;
-  const curUser = await User.findById(userId);
-  res.render("ProfileView", { curUser, curUserType: "customer" });
+    const {
+        userId
+    } = req.params;
+    const shop = req.shop;
+    const user = req.user;
+    res.render("ProfileViewCustomer", {
+        user,
+        shop
+    });
 });
 
 router.get("/:userId/cart", requiredLogIn, async (req, res) => {
-  const { userId } = req.params;
-  const curUser = await User.findById(userId);
-  const chosenUserCart = curUser.cart.slice();
-  res.render("BasketView", {
-    chosenUserCart,
-    curUser,
-    curUserType: "customer",
-  });
+    const {
+        userId
+    } = req.params;
+    const shop = req.shop;
+    const user = req.user;
+    const chosenUserCart = user.cart.slice();
+    res.render("BasketView", {
+        user,
+        shop,
+        chosenUserCart
+    });
 });
 
 router.get("/userId/noti", requiredLogIn, async (req, res) => {
-  const { userId } = req.params;
-  const curUser = await User.findById(userId);
-  
+
 });
 
 router.post("/:userId/uploadAvatar", async (req, res) => {
-  const { userId } = req.params;
-  const curUser = await User.findById(userId);
-  const storage = multer.diskStorage({
-    destination: "../src/public/upload",
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now());
-    },
-  });
+    const {
+        userId
+    } = req.params;
+    const curUser = await User.findById(userId);
+    const storage = multer.diskStorage({
+        destination: "../src/public/upload",
+        filename: function(req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now());
+        },
+    });
 
-  const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 10000000,
-    },
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb);
-    },
-  }).single("fileAva");
+    const upload = multer({
+        storage: storage,
+        limits: {
+            fileSize: 10000000,
+        },
+        fileFilter: function(req, file, cb) {
+            checkFileType(file, cb);
+        },
+    }).single("fileAva");
 
-  upload(req, res, (err) => {
-    if (err) {
-      res.render("ProfileView", {
-        curUser,
-        curUserType: "customer",
-        msg: err,
-      });
-    } else {
-      if (req.file == undefined) {
-        res.render("ProfileView", {
-          curUser,
-          curUserType: "customer",
-          msg: "ERROR: No file selected",
-        });
-      } else {
-        const img = fs.readFileSync(req.file.path);
-        const encodeImg = img.toString("base64");
-        curUser.avatar = encodeImg;
-        curUser.save();
-        res.render("ProfileView", {
-          curUser,
-          curUserType: "customer",
-          msg: req.file.filename + " uploaded",
-        });
-      }
-    }
-  });
+    upload(req, res, (err) => {
+        if (err) {
+            res.render("ProfileView", {
+                curUser,
+                curUserType: "customer",
+                msg: err,
+            });
+        } else {
+            if (req.file == undefined) {
+                res.render("ProfileView", {
+                    curUser,
+                    curUserType: "customer",
+                    msg: "ERROR: No file selected",
+                });
+            } else {
+                const img = fs.readFileSync(req.file.path);
+                const encodeImg = img.toString("base64");
+                curUser.avatar = encodeImg;
+                curUser.save();
+                res.render("ProfileView", {
+                    curUser,
+                    curUserType: "customer",
+                    msg: req.file.filename + " uploaded",
+                });
+            }
+        }
+    });
 });
 
 router.put("/:userId/profile", async (req, res) => {
-  const { username, password, email, address, tel } = req.body;
-  const { userId } = req.params;
-  const userUpdate = await User.findById(userId);
-  userUpdate.name = username;
-  userUpdate.email = email;
-  userUpdate.address = address;
-  userUpdate.phone = tel;
-  // const valid = await bcrypt.compare(password, userUpdate.password);
-  // if (!valid) {
-  //   userUpdate.password = await bcrypt.hash(password, 12);
-  // }
-  userUpdate.save();
-  res.redirect("/user/" + userId + "/profile");
+    const {
+        username,
+        password,
+        email,
+        address,
+        tel
+    } = req.body;
+    const {
+        userId
+    } = req.params;
+    const userUpdate = await User.findById(userId);
+    userUpdate.name = username;
+    userUpdate.email = email;
+    userUpdate.address = address;
+    userUpdate.phone = tel;
+    // const valid = await bcrypt.compare(password, userUpdate.password);
+    // if (!valid) {
+    //   userUpdate.password = await bcrypt.hash(password, 12);
+    // }
+    userUpdate.save();
+    res.redirect("/user/" + userId + "/profile");
 });
 
 module.exports = router;
